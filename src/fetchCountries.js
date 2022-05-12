912f432ae994afd329fffc76f0eccbae0826c82f
Larin Artem <larin_art@ukr.net>
2022-05-12 23:03:16 +0300

    Start task

---
+++ src/fetchCountries.js
@@ -0,0 +1,10 @@
+const BASE_URL = 'https://restcountries.com/v3.1/name';
+
+export const fetchCountries = (name) => {
+    return fetch(`${URL}/${name}`).then(res => {
+        if (res.ok) {
+            return res.json();
+        }
+        throw new Error(res.status);
+    });
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './css/styles.css';
+import {Notify} from 'notiflix';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/fetchCountries.js';
+import countryListTpl from './template/country-list.hbs';
+import countryCardTpl from './template/country-card.hbs';
+
 
 const DEBOUNCE_DELAY = 300;
+
+
+
