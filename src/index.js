162f0df8a0eb5e5cd45387a3cb22b16d826d233a
Larin Artem <larin_art@ukr.net>
2022-05-13 20:59:37 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,59 @@
 import './css/styles.css';
 import {Notify} from 'notiflix';
 import debounce from 'lodash.debounce';
-import { fetchCountries } from './js/fetchCountries.js';
+
+import {fetchCountries} from './js/fetchCountries.js';
 import countryListTpl from './template/country-list.hbs';
 import countryCardTpl from './template/country-card.hbs';
 
-
 const DEBOUNCE_DELAY = 300;
+const refs = {
+    input: document.querySelector('#search-box'),
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
+    clickOnCountryTitle: document.querySelector('.country-title')
+};
+
+const clearMarkup = () => {
+    refs.countryInfo.innerHTML = '';
+    refs.countryList.innerHTML = '';
+};
+
+const createCountryList = countries => countries.map(country => countryListTpl(country)).join('');
+
+const renderCountryCard = countries => {
+    const countriesQuantity = countries.length;
+    clearMarkup();
+    if (countriesQuantity > 10) {
+        Notify.warning("Too many matches found. Please enter a more specific name.")
+    }
+    else if(countriesQuantity > 1 && countriesQuantity <= 10){
+        Notify.info(`Hooray! We found ${countriesQuantity} countries.`);
+        refs.countryList.innerHTML  = createCountryList(countries);
+    } 
+    else if(countriesQuantity === 1){
+        Notify.success(`This is exactly what you were looking for!`);
+        refs.countryInfo.innerHTML = countryCardTpl(countries[0]);
+    }
+};
+    
+const onFetchError = () => {
+    Notify.failure('Oops! There is no country with that name!');
+    clearMarkup();
+};
+
+const onSearch = e => {
+    e.preventDefault;
+    if (e.target.value) {
+        fetchCountries(e.target.value.trim())
+            .then(renderCountryCard)
+            .catch(onFetchError);
+    }
+    clearMarkup();
+};
+
+refs.input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+// refs.clickOnCountryTitle.addEventListener('click', );
 
 
 
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,10 +1,13 @@
 const BASE_URL = 'https://restcountries.com/v3.1/name';
 
 export const fetchCountries = (name) => {
-    return fetch(`${URL}/${name}`).then(res => {
-        if (res.ok) {
-            return res.json();
+    return fetch(`${BASE_URL}/${name}`)
+    .then(res => {
+      if (res.ok) {
+          return res.json();
         }
         throw new Error(res.status);
-    });
+      });
 };
+
+
