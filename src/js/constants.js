63ccf89132cc477e4536696148919c5e4fa4af29
Larin Artem <larin_art@ukr.net>
2022-05-15 15:09:38 +0300

    finish

--- src/js/constants.js
+++ src/js/constants.js
@@ -3,5 +3,5 @@ export const refs = {
     input: document.querySelector('#search-box'),
     countryList: document.querySelector('.country-list'),
     countryInfo: document.querySelector('.country-info'),
-    clickOnCountryTitle: document.querySelector('.country-title')
+    // clickOnCountryTitle: document.querySelector('.country-title')
 };
